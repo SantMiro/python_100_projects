721072a0a8e041fb182278615265d94636c89bc0
SantMiro <mirot.santiago@gmail.com>
2024-06-20 16:39:50 -0600

    Add blackjack.py day 11

--- day11_blackjack.py
+++ day11_blackjack.py
@@ -1,13 +1,27 @@
+################################
+# Title: Blackjack game        #
+# Author: Santiago Miro        #
+#        June 2024             #
+################################
+
+''' This code plays a blackjack game against the pc.'''
+
 import random
 import time
 cards = {'A':11, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10}
 
 def draw_card():
+
+    '''Randomly chooses a card from the list. Saves its value and symbol.'''
     face = random.choice(card_faces)
     value = cards[face]
     return face, value
 
 def initial_setup():
+
+    ''' In order to start the game, the same process is always repeated. Draw two cards for the player and one for the dealer.
+    Sum the value of the drawn cards.'''
+
     first_draw_face, first_draw_value = draw_card()
     second_draw_face, second_draw_value = draw_card()
 
@@ -23,6 +37,10 @@ def initial_setup():
     return draws, dealer_draws, dealer_draw_value, total
 
 def player_turn(draws, total):
+
+    '''When the game starts per turn a card is drawn and then added to the total score. Tha ace can have values of '1' and '11',
+    so a condition is implemented.'''
+
     draw_face, draw_value = draw_card()
     draws.append(draw_face)
     total += draw_value
@@ -38,14 +56,14 @@ def player_turn(draws, total):
 card_faces = list(cards.keys())
 
 def blackjack_game():
-    end_game = False
+    
     new_game = input('Type "y" to start a new game of blackjack. Type "n" to cancel.')
     if new_game == 'y':
-        draws, dealer_draws, dealer_draw_value, total = initial_setup()
+        draws, dealer_draws, dealer_draw_value, total = initial_setup() # Saves lists of draws and total scores.
 
         if total < 21:
             blackjack = False
-            hit_or_stay = input('Type "y" to get another card or "n" if you want to stay. ')
+            hit_or_stay = input('Type "y" to get another card or "n" if you want to stay. ') # Draw an extra card or stay.
             stay = False
 
             if hit_or_stay == 'y':
@@ -59,43 +77,43 @@ def blackjack_game():
                         print('\nYou are over 21!\n')
                         stay = True
         else:
-            print('It is BLACKJACK!')
+            print('It is BLACKJACK!') # If initial hand is 21, then it is blackjack!
             blackjack = True
-        #print(f'Your final score is: {draws} = {total}')   
+         
         time.sleep(1)        
         print('-----------------------------------------------------------------------------------')
         print('\nIt is the dealer\'s turn.\n')
 
-        dealer_draws, dealer_draw_value = player_turn(dealer_draws,dealer_draw_value)
+        dealer_draws, dealer_draw_value = player_turn(dealer_draws,dealer_draw_value) # Dealer draws second card.
 
         lose = False
         win = False
         draw = False
-        if dealer_draw_value == 21 and blackjack == True:
+        if dealer_draw_value == 21 and blackjack == True: # Draw if both blackjack.
             draw == True    
-        elif dealer_draw_value == 21 and blackjack == False:
+        elif dealer_draw_value == 21 and blackjack == False: # Lose if only dealer blackjack.
             lose = True
-        elif dealer_draw_value < 21 and blackjack == True:
+        elif dealer_draw_value < 21 and blackjack == True: # Win if only player blackjack.
             win = True
-        elif dealer_draw_value < 21 and blackjack == False:
+        elif dealer_draw_value < 21 and blackjack == False: # If there is no blackjack, the turns are played.
             while dealer_draw_value <= 16:
                 time.sleep(1)
                 dealer_draws, dealer_draw_value = player_turn(dealer_draws,dealer_draw_value)
                 time.sleep(1)
                 if dealer_draw_value > 21:
                     print('Dealer busts!')
-            if total <= 21 and dealer_draw_value <= 21:
+            if total <= 21 and dealer_draw_value <= 21: # If both under 21, highest score wins.
                 if total > dealer_draw_value:
                     win = True
-                elif total == dealer_draw_value:
+                elif total == dealer_draw_value: # If both scores are the same then it is a draw.
                     draw = True
                 elif total < dealer_draw_value:
                     lose = True
-            elif total <= 21 and dealer_draw_value > 21:
+            elif total <= 21 and dealer_draw_value > 21: # If only one busts, the other wins.
                 win = True
             elif total > 21 and dealer_draw_value <= 21:
                 lose = True
-            elif total > 21 and dealer_draw_value > 21:
+            elif total > 21 and dealer_draw_value > 21: # If both bust, then it is a draw.
                 draw = True
 
         if win:
